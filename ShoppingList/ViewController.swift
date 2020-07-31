8592d36613e1296081b875e22085efdcec27a627
Ian Becker <ian-becker@lambdastudents.com>
2020-07-31 08:07:17 -0700

    Initial commit.

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Ian Becker on 7/31/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
